fc2233a5cc96eb0892ebe553ff6f669aa9d5285a
DhvanilP <dhvanilhparikh@gmail.com>
2019-03-22 16:51:30 +0530

    working

--- Convert/converter.py
+++ Convert/converter.py
@@ -4,10 +4,10 @@ import base64
 def fileToBase64(filename):
     with open(filename, "rb") as image_file:
         encoded_string = base64.b64encode(image_file.read())
-    return encoded_string
+    return encoded_string.decode('utf-8')
 
 
 def base64ToFile(encoded_string, outputFileName):
-    data64decode = base64.decodestring(encoded_string)
+    data64decode = base64.decodestring(encoded_string.encode('utf-8'))
     data_result = open(outputFileName, "wb")
     data_result.write(data64decode)
