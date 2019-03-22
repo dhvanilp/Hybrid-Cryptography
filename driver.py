c7bc8895fd486b8c5bc6cc594c337ee9a041cc89
DhvanilP <dhvanilhparikh@gmail.com>
2019-03-22 12:56:18 +0530

    drive r updarte

--- driver.py
+++ driver.py
@@ -42,23 +42,70 @@ def Converter_tester():
 
 
 def main():
+    AES_tester()
 
     # AES_tester()
     # ECC_tester()
     # Converter_tester()
 
-    # 1. Multimedia data -> Base64 Encoding and plain text
+    #### 1. Multimedia data -> Base64 Encoding and plain text
     multimedia_data = converter.fileToBase64("test_files/test.jpg")
 
-    # 2. Encrypt  AES_key with ECC public key
+
+    # #### Encrypt  AES_key with ECC public key
     aes_key = 0x2b7e151628aed2a6abf7158809cf4f3c
-    ecc_AESkey = ECC.ECC()
-    privKey_for_aesKey = random.getrandbits(256)
-    publicKey_for_aesKey = ecc_AESkey.gen_pubKey(privKey)
-    (C1_aesKey, C2_aesKey) = ecc_AESkey.encryption(publicKey_for_aesKey, aes_key)
-    
+    # # aes_key=str(aes_key)
+    # ecc_AESkey = ECC.ECC()
+    # privKey_for_aesKey = random.getrandbits(256)
+    # publicKey_for_aesKey = ecc_AESkey.gen_pubKey(privKey_for_aesKey)
+    # (C1_aesKey, C2_aesKey) = ecc_AESkey.encryption(publicKey_for_aesKey, str(aes_key))
+    # # decryptedAESkey = ecc_AESkey.decryption(C1_aesKey,C2_aesKey, privKey_for_aesKey)
+
+    #### 2. Encrypt the multimedia_data with AES algorithm
+    aes_multimedia_data = AES.AES(aes_key)
+    encrypted_multimedia = aes_multimedia_data.encryptBigData(multimedia_data)
+    print("This is the encrypted data:", encrypted_multimedia)
+    # decrypted_multimedia = aes_multimedia_data.decryptBigData(encrypted_multimedia)
+    # print("This is the decrypted data:", decrypted_multimedia)
+    #
+    # #### 3. Encrypt the encrypted_multimedia with ECC
+    # ecc_multimedia_data = ECC.ECC()
+    # # here the same keys are used as before
+    # (C1_multimedia, C2_multimedia) = ecc_multimedia_data.encryption(publicKey_for_aesKey, encrypted_multimedia)
+    #
+    # #### 5. Data to send to the other side:
+    # # (C1_aesKey, C2_aesKey)
+    # # (C1_multimedia, C2_multimedia)
+    # # privKey_for_aesKey
+    #
+    #
+    # ######################################################
+    # ##### This is on the receiver side
+    #
+    # #### 1. Decrypt the data with ECC
+    #
+    # ecc_multimedia_dataD = ECC.ECC()
+    # encrypted_multimedia = ecc_multimedia_dataD.decryption(C1_multimedia, C2_multimedia, privKey_for_aesKey)
+    #
+    # #### Decrypt with ECC to get the AES key
+    # ecc_AESkey = ECC.ECC()
     # decryptedAESkey = ecc_AESkey.decryption(C1_aesKey,C2_aesKey, privKey_for_aesKey)
-    
+    #
+    # #### 2. Decrypt with AES
+    # aes_multimedia_data = AES.AES(decryptedAESkey)
+    # decrypted_multimedia = aes.decryptBigData(encrypted_multimedia)
+    #
+    #
+    # #### 3. Decode from Base64 to the corresponding fileToBase64
+    # converter.base64ToFile(decrypted_multimedia, "success.jpg")
+
+
+
+
+
+
+
+
 
 
 if __name__ == "__main__":
