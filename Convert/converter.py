faf218f8dae6ae40ae33a752ad398f2db067f610
DhvanilP <dhvanilhparikh@gmail.com>
2019-03-23 01:50:06 +0530

    AES+ECC ecryption od daata working on image, audio, text file

--- Convert/converter.py
+++ Convert/converter.py
@@ -11,3 +11,23 @@ def base64ToFile(encoded_string, outputFileName):
     data64decode = base64.decodestring(encoded_string.encode('utf-8'))
     data_result = open(outputFileName, "wb")
     data_result.write(data64decode)
+
+
+def makeSingleString(bigList):
+    retData = ""
+    dollar = "$$$$$$$$$$$$$$$$$$$$$$$$$"
+    for listEle in bigList:
+        msg = str(listEle)
+        msg = msg + dollar
+        msg = msg[0:42]
+        retData = retData + msg
+    return retData
+
+
+def makeListFromString(longString):
+    retData = []
+    data = longString.split('$')
+    for item in data:
+        if len(item) > 0:
+            retData.append(int(item))
+    return retData
--- driver.py
+++ driver.py
@@ -42,98 +42,111 @@ def Converter_tester():
     converter.base64ToFile(data, "test_success.jpg")
 
 
-def makeSingleString(bigList):
-    retData = ""
-    dollar = "$$$$$$$$$$$$$$$$$$$$$$$$$"
-    for listEle in bigList:
-        msg = str(listEle)
-        msg = msg + dollar
-        msg = msg[0:42]
-        retData = retData + msg
-    return retData
-
-
-def makeListFromString(longString):
-    retData = []
-    data = longString.split('$')
-    for item in data:
-        if len(item) > 0:
-            retData.append(item)
-    return retData
-
-
 def main():
     # AES_tester()
     # ECC_tester()
     # Converter_tester()
+    VERBOSE = int(input("VERBOSE 1/0? > "))
 
+    input_file = input("Enter the name of file to encrypt present in ./test_files/ > ")
+    output_file = input("Enter the name of output file > ")
+
+    ######################################################################################
     # 1. Multimedia data -> Base64 Encoding and plain text
-    multimedia_data = converter.fileToBase64("test_files/image.jpg")
-    multimedia_data = (b"hey").decode('utf-8')
+    multimedia_data = converter.fileToBase64("test_files/" + input_file)
+    # multimedia_data = (b"hey").decode('utf-8')
+    # multimedia_data = input("Enter the data >")
+    ######################################################################################
 
-    # Encrypt  AES_key with ECC public key
     aes_key = 0x2b7e151628aed2a6abf7158809cf4f3c
-    ecc_AESkey = ECC.ECC()
-    privKey_for_aesKey = random.getrandbits(256)
-    publicKey_for_aesKey = ecc_AESkey.gen_pubKey(privKey_for_aesKey)
-    (C1_aesKey, C2_aesKey) = ecc_AESkey.encryption(
-        publicKey_for_aesKey, str(aes_key))
-    # decryptedAESkey = ecc_AESkey.decryption(C1_aesKey,C2_aesKey, privKey_for_aesKey)
 
+    ######################################################################################
+    # Encrypt  AES_key with ECC public key
+    ecc_obj_AESkey = ECC.ECC()
+    private_key = random.getrandbits(256)
+    public_key = ecc_obj_AESkey.gen_pubKey(private_key)
+    (C1_aesKey, C2_aesKey) = ecc_obj_AESkey.encryption(public_key, str(aes_key))
+    # decryptedAESkey = ecc_AESkey.decryption(C1_aesKey,C2_aesKey, private_key)
+    ######################################################################################
+
+    ######################################################################################
     # 2. Encrypt the multimedia_data with AES algorithm
-    aes_multimedia_data = AES.AES(aes_key)
-    encrypted_multimedia = aes_multimedia_data.encryptBigData(multimedia_data)
+    aes = AES.AES(aes_key)
+    encrypted_multimedia = aes.encryptBigData(multimedia_data)
+    data_for_ecc = converter.makeSingleString(encrypted_multimedia)
+    if(VERBOSE):
+        print("######################################################################################")
+        print("AES ENCRYPTION")
+        print("Encrypted data from AES:", encrypted_multimedia)
+        print("String data that is passed to ECC:", data_for_ecc)
 
-    data_for_ecc = makeSingleString(encrypted_multimedia)
+    # converter.base64ToFile(decrypted_multimedia.encode('utf-8'),"test_success.jpg")
+    ######################################################################################
 
-    # print("This is the encrypted data:", encrypted_multimedia)
-    # decrypted_multimedia = aes_multimedia_data.decryptBigData(encrypted_multimedia)
-    # print("This is the decrypted data:", decrypted_multimedia)
-    # converter.base64ToFile(decrypted_multimedia,"test_success.jpg")
-
+    ######################################################################################
     # 3. Encrypt the encrypted_multimedia with ECC
-    ecc_multimedia_data = ECC.ECC()
-    # here the same keys are used as before
-    # (C1_multimedia, C2_multimedia) = ecc_multimedia_data.encryption(publicKey_for_aesKey, encrypted_multimedia)
-    (C1_multimedia, C2_multimedia) = ecc_multimedia_data.encryption(
-        publicKey_for_aesKey, data_for_ecc)
-    print(C1_multimedia, C2_multimedia)
-    print("the above")
-    # multimedia = ecc_multimedia_data.decryption(
-    #     C1_multimedia, C2_multimedia, privKey_for_aesKey)
-    # converter.base64ToFile(multimedia, "test_success.jpg")
+    ecc = ECC.ECC()
+    (C1_multimedia, C2_multimedia) = ecc.encryption(public_key, data_for_ecc)
+    if(VERBOSE):
+        print("######################################################################################")
+        print("ECC ENCRYPTION")
+        print("Encrypted data from ECC (C1,C2):")
+        print("C1:", C1_multimedia)
+        print("C2:", C2_multimedia)
+    ######################################################################################
 
+    ######################################################################################
     # Data to send to the other side:
-
     # (C1_aesKey, C2_aesKey)
     # (C1_multimedia, C2_multimedia)
-    # privKey_for_aesKey
-    #
-    #
-    # ######################################################
-    # ##### This is on the receiver side
-    #
-    # 1. Decrypt the data with ECC
+    # private_key
+    ######################################################################################
 
-    ecc_multimedia_dataD = ECC.ECC()
-    encrypted_multimedia = ecc_multimedia_dataD.decryption(
-        C1_multimedia, C2_multimedia, privKey_for_aesKey)
+    # This is on the receiver side
 
-    clean_data_list = makeListFromString(encrypted_multimedia)
+    ######################################################################################
     # Decrypt with ECC to get the AES key
     ecc_AESkey = ECC.ECC()
-    decryptedAESkey = ecc_AESkey.decryption(
-        C1_aesKey, C2_aesKey, privKey_for_aesKey)
-    
-    print("hi")
-    print(int(hex(int(decryptedAESkey))),0)
+    decryptedAESkey = ecc_AESkey.decryption(C1_aesKey, C2_aesKey, private_key)
+    if(VERBOSE):
+        print("######################################################################################")
+        print("AES key decrypted:", decryptedAESkey)
+        print(type(decryptedAESkey))
+    ######################################################################################
 
+    ######################################################################################
+    # 1. Decrypt the data with ECC
+    ecc_obj = ECC.ECC()
+    encrypted_multimedia = ecc_obj.decryption(
+        C1_multimedia, C2_multimedia, private_key)
+    clean_data_list = converter.makeListFromString(encrypted_multimedia)
+    if(VERBOSE):
+        print("######################################################################################")
+        print("ECC Decryption")
+        print("ECC Decryption from decrpytion:", encrypted_multimedia)
+        print("Clean data from ECC decrpytion:", clean_data_list)
+    ######################################################################################
+
+    ######################################################################################
     # 2. Decrypt with AES
-    aes_multimedia_data = AES.AES(hex(decryptedAESkey))
-    decrypted_multimedia = aes_multimedia_data.decryptBigData(clean_data_list)
+    # aes_multimedia_data = AES.AES(int(hex(int(decryptedAESkey)),0))
+    aes_obj = AES.AES(int(decryptedAESkey))
+    decrypted_multimedia = aes_obj.decryptBigData(clean_data_list)
+    if(VERBOSE):
+        print("######################################################################################")
+        print("AES Decryption")
+        print("Data original:", decrypted_multimedia)
+        type(decrypted_multimedia)
+    ######################################################################################
 
+    ######################################################################################
     # 3. Decode from Base64 to the corresponding fileToBase64
-    converter.base64ToFile(decrypted_multimedia, "test_success.txt")
+    converter.base64ToFile(decrypted_multimedia, output_file)
+    delete = int(input("Delete File? 1/0:"))
+    if(delete):
+        import os
+        os.remove(output_file)
+    ######################################################################################
 
 
 if __name__ == "__main__":
